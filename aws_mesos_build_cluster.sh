cd166e4990be2028e3dd6ea711a1ecf90106d4c7
Reza Rahim <reza.rahim@gmail.com>
2016-10-31 00:02:35 -0500

    Update aws_mesos_build_cluster.sh

--- aws_mesos_build_cluster.sh
+++ aws_mesos_build_cluster.sh
@@ -20,7 +20,7 @@ cd ansible/provision
 
 ansible-playbook install_pyhton.yml -i inventory.ini
 
-ansible-playbook mesos_build_cluster.yml -i inventory.ini -e "cloud_provider=$cloud_provider" -e "aws_region=$aws_region" -e "aws_zone=$aws_zone" -e "aws_access_key_id=$AWS_ACCESS_KEY_ID" -e "aws_secret_access_key=AWS_SECRET_ACCESS_KEY"
+ansible-playbook mesos_build_cluster.yml -i inventory.ini -e "cloud_provider=$cloud_provider" -e "aws_region=$aws_region" -e "aws_zone=$aws_zone" -e "aws_access_key_id=$AWS_ACCESS_KEY_ID" -e "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY"
 
 sleep 30
 
