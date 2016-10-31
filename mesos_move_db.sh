5725bebbc79878cac8938a450d3e6e0a83f7cb5b
Reza Rahim <reza.rahim@gmail.com>
2016-10-31 00:29:54 -0500

    Update mesos_move_db.sh

--- mesos_move_db.sh
+++ mesos_move_db.sh
@@ -2,5 +2,9 @@
 
 ssh node3 sudo docker pull 10.0.15.11:5000/application/db/mongo:2.6.12
 
+curl -X DELETE 10.0.15.11:8080/v2/apps/application/db/mongo
+sleep 15
+
+
 cd ansible/mesos-app/mongo
 ./deploy_without_docker_build.sh
