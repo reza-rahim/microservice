a0c956a38ca0db9240e7ce3a903a877c74b3fbac
vagrant <vagrant@mgmt>
2016-11-26 05:38:58 +0000

    checkpoint

---
+++ ansible/mesos-analytics/delete.sh
@@ -0,0 +1,5 @@
+curl -XDELETE mrthn:8080/v2/apps/analytics/spark/spark-worker1
+curl -XDELETE mrthn:8080/v2/apps/analytics/spark/spark-master
+curl -XDELETE mrthn:8080/v2/apps//analytics/hadoop/hdfs-dn-02
+curl -XDELETE mrthn:8080/v2/apps//analytics/hadoop/hdfs-dn-01
+curl -XDELETE mrthn:8080/v2/apps//analytics/hadoop/hdfs-nn
