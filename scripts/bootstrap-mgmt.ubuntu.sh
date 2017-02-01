c6b0e79da24d68132fd9cf49a40764f9668cbc37
rerahim <rerahim@cisco.com>
2017-01-31 20:22:39 -0600

    kube cont ..

--- scripts/bootstrap-mgmt.ubuntu.sh
+++ scripts/bootstrap-mgmt.ubuntu.sh
@@ -43,13 +43,13 @@ cp -a ansible/inventory.ini /etc/ansible/hosts
 cat > /etc/hosts <<EOL
 
 # vagrant environment nodes
-127.0.0.1  localhost
-10.0.15.10 mgmt logserver hdfscli1 sparkcli1 hbasecli1 kafkacli1
-10.0.15.11 node1 zk1 nn1 sm1 zep1 hmst1 dckreg  mrthn flckctl
-10.0.15.12  node2 dn01 sw01 hrs01 kaf01
-10.0.15.13  node3 dn02 sw02 hrs02 kaf02
-10.0.15.14  node4 dn03 sw03 hrs03 kaf03
-10.0.15.15  node5 dn04 sw04 hrs04 kaf04
-10.0.15.16  node6 dn05 sw05 hrs05 kaf05
-10.0.15.17  node7 dn06 sw06 hrs06 kaf06
+127.0.0.1   localhost
+10.0.15.10  mgmt logserver dckreg
+10.0.15.11  node1 api
+10.0.15.12  node2
+10.0.15.13  node3
+10.0.15.14  node4
+10.0.15.15  node5
+10.0.15.16  node6
+10.0.15.17  node7
 EOL
