5e16d73f06a352c7b3c8bd062d1a8c3d5710a468
rerahim <rerahim@cisco.com>
2017-02-27 15:14:48 -0600

    kube staff

--- ansible/kube-app/deploy/nodeapp/docker-test/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo docker build -t dckreg:5000/nodeapp:1.3 .
-sudo docker push dckreg:5000/nodeapp:1.3
--- ansible/kube-app/kafa_spark.sh
+++ ansible/kube-app/kafa_spark.sh
@@ -29,5 +29,11 @@ sed -e "s/{{ pv_annotations }}/$PV_ANNOTATIONS/" \
     -e "s/{{ sm_disk }}/$SM_DISK/"  \
 | kubectl -s api:8080 create -f -
 
-
+cat deploy/spark/sprkwrk.yml | \
+sed -e "s/{{ pv_annotations }}/$PV_ANNOTATIONS/" \
+    -e "s/{{ pv_annotations_line }}/$PV_ANNOTATIONS_LINE/" \
+    -e "s/{{ spark_version }}/$SPARK_VERSION/" \
+    -e "s/{{ sw_replicas }}/$SW_REPLICAS/"  \
+    -e "s/{{ sw_disk }}/$SW_DISK/"  \
+| kubectl -s api:8080 create -f -
 
