0e2326a5c95b59674ad4325de6b6dc90165ac641
rerahim <rerahim@cisco.com>
2017-03-10 12:55:13 -0600

    init

--- ansible/provision/kube-build.sh
+++ ansible/provision/kube-build.sh
@@ -2,5 +2,6 @@
 ansible-playbook -i kube-inventory.ini  kube-docker-registry.yml
 ./kube-docker.sh
 ansible-playbook -i kube-inventory.ini  kube-cluster.yml
-sleep 30
+sleep 15
+sudo rm /var/tmp/addon/*
 ansible-playbook -i kube-inventory.ini  kube-addon.yml
